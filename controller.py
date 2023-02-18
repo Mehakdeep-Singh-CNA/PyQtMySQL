8e7e8f97734df68365b1444f18616f8e847a9523
mehakdeep-chhina <mehakdeep@cometrics.io>
2023-02-18 18:03:00 -0330

    First commit

---
+++ controller.py
@@ -0,0 +1,40 @@
+# getStudentInfoById
+from mysql_functions import *
+
+def addStudent(fname, lname, email):
+    sql = f"INSERT INTO `school`.`students` (`first_name`, `last_name`, `email`) VALUES ('{fname}', '{lname}', '{email}');"
+    return executeQueryAndCommit(sql)
+
+def getAllStudents():
+    sql = "SELECT * FROM school.students;"
+    return executeQueryAndReturnResult(sql)
+
+def getStudentIdsAndNames():
+    sql =f"SELECT student_id, concat(first_name, ' ', last_name) as 'Student Name' FROM school.students;"
+    return executeQueryAndReturnResult(sql)
+
+def getCourseIdsAndNames():
+    sql =f"SELECT course_id, name FROM school.courses;"
+    return executeQueryAndReturnResult(sql)    
+
+def getStudentInfoById(stuId):
+    sql = f"SELECT * FROM school.students where student_id = {stuId}; "
+    stuInfo = executeQueryAndReturnResult(sql)[1][0]
+    print('stuinfo',stuInfo)
+    data = {'sid': stuInfo[0], 'fname': stuInfo[1], 'lname': stuInfo[2], 'email': stuInfo[3]}
+    print(data)
+    return data
+
+def updateStudent(sid, fname, lname, email):
+    sql = f"UPDATE `school`.`students` SET `first_name` = '{fname}', `last_name` = '{lname}', `email` = '{email}' WHERE (`student_id` = '{sid}');"
+    return executeQueryAndCommit(sql)
+
+def deleteStudentById(sid):
+    sql = f"DELETE FROM `school`.`students` WHERE (`student_id` = '{sid}');"
+    return executeQueryAndCommit(sql)
+
+
+def registerCourseToStudent(sId, cId):
+    sql = f"INSERT INTO `school`.`enrollments` (`student_id`, `course_id`) VALUES ('{sId}', '{cId}');"
+    return executeQueryAndCommit(sql)
+
---
+++ gui.py
@@ -0,0 +1,199 @@
+import sys
+
+from PyQt6 import uic
+from PyQt6.QtWidgets import *
+from controller import *
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('home.ui', self)
+
+        self.addStudentWidgetsSetup()
+
+        self.initializeAllStudentsTable()
+
+        self.initializeUpdateStudentWidgets()
+
+        self.initializeRegisterStudentWidgets()
+    
+    def initializeRegisterStudentWidgets(self):
+        self.cboRegisterStudentInfo = self.findChild(QComboBox, 'cboRegisterStudentInfo')
+        self.cboRegisterCourseInfo = self.findChild(QComboBox, 'cboRegisterCourseInfo')
+
+        self.btnRegisterStu = self.findChild(QPushButton, 'btnRegisterStu')
+        self.btnRegisterStu.clicked.connect(self.btnRegisterStuClickHandler)
+
+        self.registerStuLbl = self.findChild(QLabel, 'registerStuLbl')
+
+        colNames, rows = getStudentIdsAndNames()
+        print(colNames, rows)
+        for row in rows:
+            self.cboRegisterStudentInfo.addItem(row[1], userData=row[0])
+        # self.cboRegisterStudentInfo.currentIndexChanged.connect(self.cboStudentRegistrationCurrentIndexChangedHandler)
+
+        colNames, rows = getCourseIdsAndNames()
+        print(colNames, rows)
+        for row in rows:
+            self.cboRegisterCourseInfo.addItem(row[1], userData=row[0])
+        # self.cboRegisterCourseInfo.currentIndexChanged.connect(self.cboCourseRegistrationCurrentIndexChangedHandler)
+
+    def btnRegisterStuClickHandler(self):
+        try:
+            stuId = self.cboRegisterStudentInfo.currentData()
+            courseId = self.cboRegisterCourseInfo.currentData()
+            result = registerCourseToStudent(stuId,courseId)
+        except Exception as e:
+            self.registerStuLbl.setText(str(e))   
+
+        else:     
+            if result == 1:
+                self.registerStuLbl.setText('Success')
+            else:
+                self.registerStuLbl.setText('Failure')
+
+    def initializeAllStudentsTable(self):
+        self.tblAllStudents = self.findChild(QTableWidget, 'tblAllStudents')
+        colNames, data = getAllStudents()
+        self.displayDataInTable(colNames, data, self.tblAllStudents)
+    
+    def initializeUpdateStudentWidgets(self):
+        self.cboStudentInfo = self.findChild(QComboBox, 'cboStudentInfo')
+        self.txtUpdateStuId = self.findChild(QLineEdit, 'txtUpdateStuId')
+        self.txtUpdateStuFname = self.findChild(QLineEdit, 'txtUpdateStuFname')
+        self.txtUpdateStuLName = self.findChild(QLineEdit, 'txtUpdateStuLName')
+        self.txtUpdateStuEmail = self.findChild(QLineEdit, 'txtUpdateStuEmail')
+        self.lblModifyStuFeedback = self.findChild(QLabel, 'lblModifyStuFeedback')
+        self.btnUpdateStu = self.findChild(QPushButton, 'btnUpdateStu')
+        self.btnUpdateStu.clicked.connect(self.btnUpdateStuClickHandler)
+        self.btnDeleteStu = self.findChild(QPushButton, 'btnDeleteStu')
+        self.btnDeleteStu.clicked.connect(self.btnDeleteStuClickHandler)
+    
+        colNames, rows = getStudentIdsAndNames()
+        print(colNames, rows)
+        for row in rows:
+            self.cboStudentInfo.addItem(row[1], userData=row[0])
+        self.cboStudentInfo.currentIndexChanged.connect(self.cboStudentInfoCurrentIndexChangedHandler)
+        
+        # self.refreshStudentComboBox()
+    
+    def cboStudentInfoCurrentIndexChangedHandler(self):
+        self.refreshStudentComboBox()
+    
+    def refreshStudentComboBox(self):
+        try:
+            stuId = self.cboStudentInfo.currentData()
+            info = getStudentInfoById(stuId)
+            print("info",info)
+            self.txtUpdateStuId.setText(str(info['sid']))
+            self.txtUpdateStuFname.setText(info['fname'])
+            self.txtUpdateStuLName.setText(info['lname'])
+            self.txtUpdateStuEmail.setText(info['email'])
+        except Exception as e:
+            print(e)
+    
+    def btnUpdateStuClickHandler(self):
+        sid = self.txtUpdateStuId.text()
+        fname = self.txtUpdateStuFname.text()
+        lname = self.txtUpdateStuLName.text()
+        email = self.txtUpdateStuEmail.text()
+        result = updateStudent(sid, fname, lname, email)
+        index = self.cboStudentInfo.currentIndex()
+        if result == 1:
+            self.lblModifyStuFeedback.setText('Success')
+        else:
+            self.lblModifyStuFeedback.setText('Failure')
+        self.refreshAllTabs()
+        self.cboStudentInfo.setCurrentIndex(index)
+
+
+    def btnDeleteStuClickHandler(self):
+        try:
+            fname = self.txtUpdateStuFname.text()
+            lname = self.txtUpdateStuLName.text()
+    
+            msg = QMessageBox(self)
+            msg.setWindowTitle("Delete Confirmation")
+            msg.setText(f"Are you sure you want to delete {fname} {lname}")
+            msg.setStandardButtons(QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No)
+            msg.setIcon(QMessageBox.Icon.Question)
+            button = msg.exec()
+            if button == QMessageBox.StandardButton.Yes:
+                sid = self.txtUpdateStuId.text()
+                result = deleteStudentById(sid)
+                if result == 1:
+                    self.lblModifyStuFeedback.setText('Success')
+                    self.refreshAllTabs()
+                else:
+                    self.lblModifyStuFeedback.setText('Failure')
+            elif button == QMessageBox.StandardButton.No:
+                self.lblModifyStuFeedback.setText('Delete Cancelled')
+    
+        except Exception as e:
+            if "1451 (23000): " in str(e):
+                self.lblModifyStuFeedback.setText('First Delete Enrollments')
+        self.refreshStudentComboBox()
+    
+    def displayDataInTable(self, columns, rows, table:QTableWidget):
+        """
+        Displays the data in the rows in the table provided
+        """
+        table.setRowCount(len(rows))
+        table.setColumnCount(len(columns))
+        for i in range(len(rows)): # once for each row
+            row = rows[i]
+            for j in range(len(row)): # once for each cell in a given row
+                table.setItem(i, j, QTableWidgetItem(str(row[j])))
+        columns = ['ID', 'First Name', 'Last Name', 'Email']
+        for i in range(table.columnCount()):
+            table.setHorizontalHeaderItem(i, QTableWidgetItem(f'{columns[i]}'))
+    
+    
+    def addStudentWidgetsSetup(self):
+        # widgets for add student
+        self.lblAddStudentFeedback = self.findChild(QLabel, 'lblAddStudentFeedback')
+        self.txtAddStuFName = self.findChild(QLineEdit, 'txtAddStuFName')
+        self.txtAddStuLName = self.findChild(QLineEdit, 'txtAddStuLName')
+        self.txtAddStuEmail = self.findChild(QLineEdit, 'txtAddStuEmail')
+        self.btnAddStudent = self.findChild(QPushButton, 'btnAddStudent')
+        self.btnAddStudent.clicked.connect(self.btnAddStudentClickHandler)
+    
+    def btnAddStudentClickHandler(self):
+        try:
+            fname = self.txtAddStuFName.text()
+            assert fname != '', "First Name is mandatory"
+            lname = self.txtAddStuLName.text()
+            email = self.txtAddStuEmail.text()
+            result = addStudent(fname, lname, email)
+        except Exception as e:
+            self.lblAddStudentFeedback.setText(str(e))
+        else:
+            if result == 1:
+                self.lblAddStudentFeedback.setText("Student Added")
+            else:
+                self.lblAddStudentFeedback.setText("Student could not be added")
+        self.refreshAllTabs() 
+
+    def refreshAllStudentTab(self):
+        colNames, data = getAllStudents()
+        self.displayDataInTable(colNames, data, self.tblAllStudents)
+
+    def refreshUpdateStudentTab(self):
+        colNames, rows = getStudentIdsAndNames()
+        print(colNames, rows)
+        self.cboStudentInfo.clear()
+
+        for row in rows:
+            self.cboStudentInfo.addItem(row[1], userData=row[0])
+        self.cboStudentInfo.currentIndexChanged.connect(self.cboStudentInfoCurrentIndexChangedHandler)
+
+    def refreshAllTabs(self):
+        self.refreshAllStudentTab()
+        self.refreshUpdateStudentTab()             
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    app.exec()
